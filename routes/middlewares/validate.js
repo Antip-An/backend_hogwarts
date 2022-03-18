5beaf6d6c35c7bccc8f21effbe24414df33e595a
Antip-An <anechka-antipina@mail.ru>
2022-03-18 13:09:59 +0300

    contrl-seed-test-todoRoutes

---
+++ routes/middlewares/validate.js
@@ -0,0 +1,13 @@
+//TODO:
+const { validationResult } = require("express-validator");
+
+const validate = () => (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.send({ success: false, code: "WRONG_REQUEST", errors: errors.array() });
+    return;
+  }
+  next();
+};
+
+module.exports = validate;
